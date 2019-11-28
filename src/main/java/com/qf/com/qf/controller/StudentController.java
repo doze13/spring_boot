c882f681faafea56a44886de274e1377040ada5c
PatyQ <qxy136181378@gmail.com>
2019-11-28 18:00:38 +0800

    wqewqewq

--- src/main/java/com/qf/com/qf/controller/StudentController.java
+++ src/main/java/com/qf/com/qf/controller/StudentController.java
@@ -8,13 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.sound.midi.Soundbank;
 import java.util.List;
 
 /**
- *
+ *学生管理系统
  */
 @Controller
 @RequestMapping("/stu")
